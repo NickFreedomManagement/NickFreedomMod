e76ae09044b09e5fe0c3c6a008020b3c2fbf31d5
๖ۣۜItsNickx <itsnick.roberts@gmail.com>
2016-01-25 19:57:17 +1100

    Create Command_nfm.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_nfm.java
@@ -0,0 +1,73 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.Config.TFM_ConfigEntry;
+import me.StevenLawson.TotalFreedomMod.Config.TFM_MainConfig;
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_BanManager;
+import me.StevenLawson.TotalFreedomMod.TFM_CommandBlocker;
+import me.StevenLawson.TotalFreedomMod.TFM_Log;
+import me.StevenLawson.TotalFreedomMod.TFM_PermbanList;
+import me.StevenLawson.TotalFreedomMod.TFM_PlayerList;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/*
+ * See https://github.com/TotalFreedom/License - This file may not be edited or removed.
+ */
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
+@CommandParameters(description = "Shows information about NickFreedomMod or reloads it", usage = "/<command> [reload]")
+public class Command_nfm extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 1)
+        {
+            if (!args[0].equals("reload"))
+            {
+                return false;
+            }
+
+            if (!TFM_AdminList.isSuperAdmin(sender))
+            {
+                playerMsg(TFM_Command.MSG_NO_PERMS);
+                return true;
+            }
+
+            TFM_MainConfig.load();
+            TFM_AdminList.load();
+            TFM_PermbanList.load();
+            TFM_PlayerList.load();
+            TFM_BanManager.load();
+            TFM_CommandBlocker.load();
+
+            final String message = String.format("%s v%s.%s reloaded.",
+                    TotalFreedomMod.pluginName,
+                    TotalFreedomMod.pluginVersion,
+                    TotalFreedomMod.buildNumber);
+
+            playerMsg(message);
+            TFM_Log.info(message);
+            return true;
+        }
+
+        playerMsg("NickFreedomMod for 'Nick Freedom', the forked all-op server, forked from TotalFreedom.", ChatColor.GOLD);
+        playerMsg(String.format("Version "
+                + ChatColor.BLUE + "%s.%s" + ChatColor.GOLD + ", built "
+                + ChatColor.BLUE + "%s" + ChatColor.GOLD + " by "
+                + ChatColor.BLUE + "%s" + ChatColor.GOLD + ".",
+                TotalFreedomMod.pluginVersion,
+                TotalFreedomMod.buildNumber,
+                TotalFreedomMod.buildDate,
+                TotalFreedomMod.buildCreator), ChatColor.GOLD);
+        playerMsg("Running on " + TFM_ConfigEntry.SERVER_NAME.getString() + ".", ChatColor.GOLD);
+        playerMsg("Created by Madgeek1450 and Prozza. Later forked by ItsTeeqo_ (_ItsNick_) to make NickFreedom", ChatColor.GOLD);
+
+        return true;
+    }
+}
+Status API Training Shop Blog About Pricing
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
